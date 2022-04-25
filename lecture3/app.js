f96ecd9fbb8a9ce8860ce9ed368f8f98bdcc1550
ygoyal25 <goyal.yashendra@gmail.com>
2022-04-25 08:30:53 +0530

    add lecture 3

--- lecture3/app.js
+++ lecture3/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const todos = [];
 
 // Set the template engine
 app.set('view engine', 'ejs');
@@ -8,19 +11,28 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // app.set('views', __dirname + '/views'); // Paths are created differently in different OS 
 
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Route -- 
 // GET, POST, PUT, PATCH, DELETE
 
 app.get('/', function (req, res) {
-    res.render('download.ejs');
+    res.render('homepage.ejs', { todos });
+});
+
+app.post('/', function(req, res) {
+    // console.log(req.body);
+    todos.push(req.body.todo);
+    res.redirect('/');
+    // res.render('homepage.ejs', { todos });
 })
 
-app.get('/test', function(req, res) {
-    res.send("This is test file");
-})
-
-app.get('*', function(req, res) {
-    res.send("Page Not Found");
+app.get('/delete/:index', function(req, res) {
+    console.log(req.params.index);
+    // Alter the array, remove the index element from array
+    res.redirect('/');
 })
 
 app.listen(3000)
